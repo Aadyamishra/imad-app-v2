682c047a92b531625704a5a984ff83e679d7bcf7
Aadyamishra <palakm1107@gmail.com>
2017-02-22 09:57:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,7 @@
 console.log('Loaded!');
 
-var img = document.getElementById ('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-
- img.onclick = function () {
-    var interval = setInterval (moveRight, 50);
+var img = document.getElementById('madi');
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+    
 };
