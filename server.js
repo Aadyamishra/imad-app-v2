2cd4988461b536c73fafdb10c8c9a60089d8ef65
Aadyamishra <palakm1107@gmail.com>
2017-02-24 10:06:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,49 @@ var articleOne = {
     title: 'first article | meenakshi',
     heading: 'article-one',
     date: '10/feb/2017',
+    content: `
+            <p>
+                Hello, Finally I also made some changes successfully. Thanks alot for teaching us this thing.
+            </p>`
+    };
     
-};
+function  createTemplate(data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content; 
+
+var htmltemplate =  `
+    <!DOCTYPE html>
+    <head>
+        <title>
+            ${title}
+        </title>
+         <meta name="viewport" content ="width=device-width,initial-scale=1"/>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class = "container">
+        <div>
+            <a href= "/">Home </a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+           ${date}
+        </div>
+        <div>
+           ${content}
+        </div>
+        </div>
+    </body>
+    </html>    
+    
+`;
+return htmltemplate; 
+}
 
 app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
