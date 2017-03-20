dff94c562001b4ee9b758e1df750dcd38713a763
Aadyamishra <palakm1107@gmail.com>
2017-03-20 15:26:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,9 @@ img.onclick = function () {
     img.style.marginLeft = '100px';
     
 };
+var user=prompt("are you sure you want to write");
+if(user==="yes") {
+    console.log("well done");
+    } else {
+        console.log("sorry");
+    }
