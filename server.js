3f3c2756b5af71a43296624c319cf82c945cc940
Aadyamishra <palakm1107@gmail.com>
2017-03-21 17:44:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,16 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+$.ajax({
+  url: "/api/getWeather",
+  data: {
+    zipcode: 97201
+  },
+  success: function( result ) {
+    $( "#weather-temp" ).html( "<strong>" + result + "</strong> degrees" );
+  }
+});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
